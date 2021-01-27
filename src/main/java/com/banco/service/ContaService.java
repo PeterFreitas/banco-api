57d9cf34814da4df779c146bc419a0f03051edbf
Peter <freitaspeteroliveira@gmail.com>
2021-01-27 16:08:40 -0300

    Simplificando o Service

--- src/main/java/com/banco/service/ContaService.java
+++ src/main/java/com/banco/service/ContaService.java
@@ -1,10 +1,33 @@
 package com.banco.service;
 
-import java.util.List;
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.banco.model.Conta;
+import com.banco.repository.ContaRepository;
 
-public interface ContaService {
-	List<Conta> buscar();
-	Conta criar(Conta novaCconta);
+@Service
+public class ContaService {
+
+	@Autowired
+	private ContaRepository contaRepository;
+
+	@Transactional
+	public Conta criar(Conta novaConta) {
+		validar(novaConta);
+		return contaRepository.save(novaConta);
+	}
+
+	private void validar(Conta novaConta) {
+		Boolean existeCpf = contaRepository.existsByCpf(novaConta.getCpf());
+		if (existeCpf) {
+			throw new RuntimeException("CPF já existe!");
+		}
+		Boolean existeEmail = contaRepository.existsByEmail(novaConta.getEmail());
+		if (existeEmail) {
+			throw new RuntimeException("Email já existe!");
+		}
+	}	
 }
